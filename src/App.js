67bd9f7f68c6966cc5c6a035e7d0e618cb1dc9a3
netkiko <netkiko@gmail.com>
2018-09-20 10:32:58 +0800

    Added Timer and Todo App

--- src/App.js
+++ src/App.js
@@ -17,6 +17,111 @@ const Image = (props) => {
   )
 }
 
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}
+      </div>
+    );
+  }
+}
+
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { seconds: 0 };
+  }
+
+  tick() {
+    this.setState(state => ({
+      seconds: state.seconds + 1
+    }));
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        Seconds: {this.state.seconds}
+      </div>
+    );
+  }
+}
+
+//ReactDOM.render(<Timer />, mountNode);
+
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>TODO</h3>
+        <TodoList items={this.state.items} />
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo">
+            What needs to be done?
+          </label>
+          <input
+            id="new-todo"
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>
+            Add #{this.state.items.length + 1}
+          </button>
+        </form>
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }
+}
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+//ReactDOM.render(<TodoApp />, mountNode);
+
 class App extends Component {
   render() {
     return (
@@ -29,6 +134,9 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Hello/>
+        <Timer/>
+        <HelloMessage name="Junnel Teves" />
+        <TodoApp/>
         <Image src1='https://www.w3schools.com/html/pic_trulli.jpg' title='Italian Trulli' width='310.93px' height='300px'/>
         <Image src1='https://www.w3schools.com/html/img_girl.jpg' title='Girl in a Jacket' width='310.93px' height='300px'/>
         <Image src1='https://www.w3schools.com/html/img_chania.jpg' title='Flowers in Chania' width='310.93px' height='300px'/>
